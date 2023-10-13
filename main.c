2327fb7fdc72be0055422b6053fb5db29780d872
cookb7 <cookb7@oregonstate.edu>
2023-10-13 16:11:37 -0600

    Added functions to create file and create repo

--- main.c
+++ main.c
@@ -1,30 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int main() {
-    FILE *file;
-    FILE *outfile;
+#include <unistd.h>
 
 
-    file = fopen("text.txt", "r");
+
+int create_file() {
+    FILE *file = fopen("/home/brendan/Templates/python_main_template.txt", "r");
     if (file == NULL) {
-        printf("Failed to open input file.\n");
+        printf("File not found or unable to open.\n");
         return 1;
     }
 
-    outfile = fopen("output.txt", "w");
-    if (file == NULL) {
-        printf("Failed to open output file.\n");
+    FILE *py_file = fopen("main.py", "w");
+    if (py_file == NULL) {
+        printf("Unable to open Python file.\n");
         return 1;
     }
 
     char c;
-    while ((c = fgetc(file)) != EOF) {
-        fputc(c, outfile);
-    }
 
+    while ((c = fgetc(file)) != EOF) {
+        fputc(c, py_file);
+    }
     fclose(file);
-    fclose(outfile);
+    fclose(py_file);
 
     return 0;
+}
+
+int create_git_repo() {
+    const char* repo_path = "/home/brendan/Documents";
+
+    if (chdir(repo_path) == 0) {
+
+        int result = system("git init new_repo");
+
+        if (result == 0) {
+            printf("Git repo created at %s\n", repo_path);
+        } else {
+            printf("Failed to create repo.\n");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int move_file() {
+    const char* repo_path = "/home/brendan/Documents/new_repo";
+
+    
+}
+
+
+int main() {
+    create_git_repo();
+    create_file();
 }
