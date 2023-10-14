985dd78529c87530ae25fd8c9cf4da4cab7fcdea
cookb7 <cookb7@oregonstate.edu>
2023-10-14 12:40:09 -0600

    Progam creates new repo adds a python file to it

--- main.c
+++ main.c
@@ -11,6 +11,20 @@ int create_file() {
         return 1;
     }
 
+    char current_path[1024];
+    if (getcwd(current_path, sizeof(current_path)) == NULL) {
+        perror("getcwd() error\n");
+        return 1;
+    }
+
+    const char* repo_path = "/home/brendan/Documents/new_repo";
+    if (chdir(repo_path) == 0) {
+        printf("Directory changed.\n");
+    } else {
+        printf("Unable to change directory.\n");
+        return 1;
+    }
+
     FILE *py_file = fopen("main.py", "w");
     if (py_file == NULL) {
         printf("Unable to open Python file.\n");
@@ -25,11 +39,24 @@ int create_file() {
     fclose(file);
     fclose(py_file);
 
+    if (chdir(current_path) == 0) {
+        printf("Directory changed back.\n");
+    } else {
+        printf("Unable to change directory back.\n");
+        return 1;
+    }
+
     return 0;
 }
 
 int create_git_repo() {
     const char* repo_path = "/home/brendan/Documents";
+    char current_path[1024];
+
+    if (getcwd(current_path, sizeof(current_path)) == NULL) {
+        perror("getcwd() error\n");
+        return 1;
+    }
 
     if (chdir(repo_path) == 0) {
 
@@ -41,16 +68,17 @@ int create_git_repo() {
             printf("Failed to create repo.\n");
             return 1;
         }
+
+        if (chdir(current_path) != 0) {
+            printf("Failed to change back to current directory.\n");
+        }
+    } else {
+        printf("Failed to change to repo destination.\n");
+        return 1;
     }
+
     return 0;
 }
-
-int move_file() {
-    const char* repo_path = "/home/brendan/Documents/new_repo";
-
-    
-}
-
 
 int main() {
     create_git_repo();
